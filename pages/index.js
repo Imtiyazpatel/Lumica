9b942f7fe1255fc8ec6f6e21404f9fd3dad53b65
Imtiyazpatel <imtiyazpatel143123@gmail.com>
2025-06-17 13:05:41 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Welcome to Lumica</h1>
+    </div>
+  );
+}
